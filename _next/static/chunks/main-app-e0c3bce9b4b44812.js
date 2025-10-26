ac028a038e9adac836fa01d808daee9f4c40b49d
Wealthometer <wealthhajoh87@gmail.com>
2025-10-26 07:57:26 +0100

    Create main-app-e0c3bce9b4b44812.js

---
+++ _next/static/chunks/main-app-e0c3bce9b4b44812.js
@@ -0,0 +1,20 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[744], {
+    432: function(e, n, t) {
+        Promise.resolve().then(t.t.bind(t, 9370, 23)),
+        Promise.resolve().then(t.t.bind(t, 6318, 23)),
+        Promise.resolve().then(t.t.bind(t, 9722, 23)),
+        Promise.resolve().then(t.t.bind(t, 8900, 23)),
+        Promise.resolve().then(t.t.bind(t, 8452, 23)),
+        Promise.resolve().then(t.t.bind(t, 2494, 23))
+    }
+}, function(e) {
+    var n = function(n) {
+        return e(e.s = n)
+    };
+    e.O(0, [918, 963], function() {
+        return n(2070),
+        n(432)
+    }),
+    _N_E = e.O()
+}
+]);
